e5e3aa3fd13d517c458cef881381eeed335a1741
서재은 <wodms5522@naver.com>
2023-05-01 21:48:06 +0900

    CRUD 기초

--- src/main/java/com/sesac/board/controller/BoardController.java
+++ src/main/java/com/sesac/board/controller/BoardController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -21,6 +23,7 @@ public class BoardController {
     @Autowired
     BoardService boardService;
 
+    // List 조회
     @GetMapping("/list")
     public String doBoardList(Model model) {
 
@@ -29,6 +32,54 @@ public class BoardController {
         model.addAttribute("list", list);
 
         return "/board/board_list";
+    }
+
+    // 입력
+    @GetMapping("/insert")
+    public String doInsert() {
+        return "/board/board_insert";
+    }
+
+    // 등록하기 실행
+    @PostMapping("/insert_exe")
+    public String doInsertExe(@ModelAttribute Board board) {
+
+        board.setDay(LocalDateTime.now());
+        boardService.doInsert(board);
+
+        return "redirect:/board/list";
+    }
+
+    // 수정
+    @GetMapping("/modify")
+    public String doModify(HttpServletRequest request, Model model) {
+        String strId = request.getParameter("id");
+
+        Board board = boardService.doSelectOne(Integer.parseInt(strId));
+
+        //request.setAttribute("vo_board", board);
+        model.addAttribute("vo_board", board);
+
+        return "/board/board_modify";
+    }
+
+    // 수정하기 실행
+    @PostMapping("/modify_exe")
+    public String doModifyExe(@ModelAttribute Board board) {
+
+        boardService.doInsert(board);
+
+        return "redirect:/board/list";
+    }
+
+
+    // 삭제
+    @GetMapping("/delete")
+    public String doDelete(@RequestParam(value="id", defaultValue = "--") String strId) {
+
+        boardService.doDelete(Integer.parseInt(strId));
+
+        return "redirect:/board/list";
     }
 
 }
